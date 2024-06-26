4187f6b78ca47b7f66008de39a72e8180a9e23e6
danishamov <122825234+danishamov@users.noreply.github.com>
2024-06-26 18:01:26 +0100

    Add React Router to the app
    Implement a Router in the App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header";
+import Home from "./components/home/Homae";
 
 function App() {
     return (
         <div id="box">
             <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
         </div>
     );
 }
