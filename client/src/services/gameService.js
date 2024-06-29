0f457065d60d8d266392f2d7689c5cda885caa28
danishamov <122825234+danishamov@users.noreply.github.com>
2024-06-29 20:43:01 +0100

    Create game service

---
+++ client/src/services/gameService.js
@@ -0,0 +1,14 @@
+const baseUrl = "http://localhost3030/jsonstore";
+
+export const create = async (data) => {
+    const response = await fetch(`${baseUrl}/games`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    const result = await response.json();
+
+    return result;
+};
