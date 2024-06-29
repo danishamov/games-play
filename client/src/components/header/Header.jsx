fa8cffbcca06805c62c0e2bb869fd7ee35a401aa
danishamov <122825234+danishamov@users.noreply.github.com>
2024-06-29 17:55:09 +0100

    Add game create page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
                 <Link to="/games">All games</Link>
                 {/* <!-- Logged-in users --> */}
                 <div id="user">
-                    <Link to="/games/create">Create Game</Link>
+                    <Link to="/game/create">Create Game</Link>
                     <Link to="/logout">Logout</Link>
                 </div>
                 {/* <!-- Guest users --> */}
