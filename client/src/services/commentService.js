b38850906f09d2ef27c401d299d9ebdd7b51ea2e
danishamov <122825234+danishamov@users.noreply.github.com>
2024-06-30 23:13:43 +0100

    Add small corrections

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -2,10 +2,16 @@ import * as request from "../lib/request";
 
 const baseUrl = "http://localhost:3030/jsonstore/coments";
 
-export const getAll = async () => {
-    const result = await request.get(baseUrl);
+export const getAll = async (gameId) => {
+    const query = new URLSearchParams({
+        where: `gameId="${gameId}"`,
+    });
 
-    return Object.values(result);
+    const result = await request.get(baseUrl);
+    // const result = await request.get(`${baseUrl}?${query}`);
+
+    //TODO: temp solution until migration to collection service
+    return Object.values(result).filter((comment) => comment.gameId === gameId);
 };
 
 export const create = async (gameId, username, text) => {
