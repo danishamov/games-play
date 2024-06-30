e9c20f34811ccdc1c292291dab81a6bf3889ac48
danishamov <122825234+danishamov@users.noreply.github.com>
2024-06-30 12:28:47 +0100

    modify requester

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,5 +1,19 @@
-export const request = async (method, url, data) => {
+const buildOptions = (data) => {
+    const options = {};
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            "content-type": "application/json",
+        };
+    }
+
+    return options;
+};
+
+const request = async (method, url, data) => {
     const response = await fetch(url, {
+        ...buildOptions(data),
         method,
     });
 
@@ -10,3 +24,11 @@ export const request = async (method, url, data) => {
 
     return result;
 };
+
+//partial application
+
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const del = request.bind(null, "DELETE");
+export const patch = request.bind(null, "PATCH");
