8c715f7620dc3501c771790c92576af8624f89e5
danishamov <122825234+danishamov@users.noreply.github.com>
2024-06-30 13:35:45 +0100

    Add game detail s page

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -8,6 +8,12 @@ export const getAll = async () => {
     return Object.values(result);
 };
 
+export const getOne = async (gameId) => {
+    const result = await request.get(`${baseUrl}/${gameId}`);
+
+    return result;
+};
+
 export const create = async (data) => {
     const result = await request.post(baseUrl, data);
     // const response = await fetch(baseUrl, {
